9ce1a3936e44e1278ad31154ae78f894d841767d
syoyu <syo.kyusyu.0725@gmail.com>
2024-07-13 22:27:42 +0900

    Update streamlit_ap.py
    Pandasの導入

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -18,9 +19,13 @@ st.write('The name on your Smoothie will be', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
+#snowpark Dataframeからpandas dataframeに変更
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 #複数選択ボックス
 ingredients_list = st.multiselect(
