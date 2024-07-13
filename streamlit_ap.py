ffee844d9137e88be75cb3193c47bf4c4c5c5283
syoyu <syo.kyusyu.0725@gmail.com>
2024-07-13 22:24:13 +0900

    Update streamlit_ap.py
    検索名も表示に追加

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -17,8 +17,9 @@ st.write('The name on your Smoothie will be', name_on_order)
 #フルーツテーブルを表示
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True) --描画の指示
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 
 #複数選択ボックス
